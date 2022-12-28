41c393abcb4d3c1c321eed41aad67628f4a80e1f
fkstndnjs <fkstndnjs@naver.com>
2022-12-28 21:15:09 +0900

    fix

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import Mail from 'nodemailer/lib/mailer';
+import * as nodemailer from 'nodemailer';
+
+interface EmailOptions {
+  to: string;
+  subject: string;
+  html: string;
+}
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'yoo.sh@datamond.ai',
+        pass: 'gqejodlfxspryhmf',
+      },
+    });
+  }
+
+  async sendMemberJoinVerification(email: string, signupVerifyToken: string) {
+    const baseUrl = 'http://localhost:8000';
+
+    const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
+
+    const mailOptions: EmailOptions = {
+      to: email,
+      subject: '테스트 가입 인증 메일',
+      html: `<form action="${url}" method="POST">
+    <button>가입확인</button>
+        </form>
+    `,
+    };
+
+    return await this.transporter.sendMail(mailOptions);
+  }
+}
