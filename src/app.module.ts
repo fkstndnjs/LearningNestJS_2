561e7f3edf9688f754e26c5d262ea54c40c5814b
YuSeokHyun <fkstndnjs@naver.com>
2022-12-28 18:06:46 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EmailService } from './email/email.service';
 
 @Module({
   imports: [UsersModule],
