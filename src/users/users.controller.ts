3e7d8381ed48efe38aa31c9d7d6f1507bb5b3318
YuSeokHyun <fkstndnjs@naver.com>
2022-12-28 16:14:39 +0900

    fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Post, Body, Query } from '@nestjs/common';
+import { Controller, Post, Body, Query, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+    return;
   }
 
-  @Post()
-  async verifyEmail(@Query() dto: VerifyEmailDto) {}
+  @Post('email-verify')
+  async verifyEmail(@Query() query: VerifyEmailDto) {
+    console.log(query);
+    return;
+  }
+
+  @Post('login')
+  async login(@Body() body: UserLoginDto) {
+    console.log(body);
+    return;
+  }
+
+  @Get(':id')
+  async getUserInfo(@Param('id') userId: string) {
+    console.log(userId);
+    return;
+  }
 }
