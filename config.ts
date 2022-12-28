7ef7720fe0a147024a76460a07c55fa5bd780c97
fkstndnjs <fkstndnjs@naver.com>
2022-12-28 22:18:31 +0900

    fix

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  email: {
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_AUTH_USER,
+      pass: process.env.EMAIL_AUTH_PASSWORD,
+    },
+    baseUrl: process.env.EMAIL_BASE_URL,
+  },
+};
