59f2a28a0f39e13be3aee7ebd14fbe79935fc0a0
fkstndnjs <fkstndnjs@naver.com>
2022-12-28 22:29:41 +0900

    fix

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Mail from 'nodemailer/lib/mailer';
 import * as nodemailer from 'nodemailer';
+import { config } from 'config';
 
 interface EmailOptions {
   to: string;
@@ -14,16 +15,16 @@ export class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: process.env.EMAIL_SERVICE,
       auth: {
-        user: 'yoo.sh@datamond.ai',
-        pass: 'gqejodlfxspryhmf',
+        user: process.env.EMAIL_AUTH_USER,
+        pass: process.env.EMAIL_AUTH_PASSWORD,
       },
     });
   }
 
   async sendMemberJoinVerification(email: string, signupVerifyToken: string) {
-    const baseUrl = 'http://localhost:8000';
+    const baseUrl = process.env.EMAIL_BASE_URL;
 
     const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
 
