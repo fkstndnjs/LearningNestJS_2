5f5ea4ca40e391f180586a137fcc64b0253c572d
YuSeokHyun <fkstndnjs@naver.com>
2022-12-28 16:16:36 +0900

    fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,31 +3,32 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './dto/user-info.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async createUser(@Body() body: CreateUserDto) {
+  async createUser(@Body() body: CreateUserDto): Promise<void> {
     console.log(body);
     return;
   }
 
   @Post('email-verify')
-  async verifyEmail(@Query() query: VerifyEmailDto) {
+  async verifyEmail(@Query() query: VerifyEmailDto): Promise<string> {
     console.log(query);
     return;
   }
 
   @Post('login')
-  async login(@Body() body: UserLoginDto) {
+  async login(@Body() body: UserLoginDto): Promise<string> {
     console.log(body);
     return;
   }
 
   @Get(':id')
-  async getUserInfo(@Param('id') userId: string) {
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
     console.log(userId);
     return;
   }
