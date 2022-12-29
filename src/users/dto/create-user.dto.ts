054f76b75d0a0a5635b6dd8d328c814ba44cffe9
YuSeokHyun <fkstndnjs@naver.com>
2022-12-29 14:17:12 +0900

    fix

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,15 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
   name: string;
 
+  @IsString()
+  @MinLength(3)
   password: string;
 
+  @IsEmail()
   email: string;
 }
