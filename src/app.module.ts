2c7dba7920a61f07bad348f1b391de20cd9d8e06
YuSeokHyun <fkstndnjs@naver.com>
2022-12-29 11:31:29 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import emailConfig from './config/emailConfig';
 import { UsersModule } from './users/users.module';
+
+console.log('🚀 -------------------------🚀');
+console.log('🚀 ~ __dirname', __dirname);
+console.log('🚀 -------------------------🚀');
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.prod.env',
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? `${__dirname}/config/env/.dev.env`
+          : `${__dirname}/config/env/.prod.env`,
+
       load: [emailConfig],
       isGlobal: true,
     }),
