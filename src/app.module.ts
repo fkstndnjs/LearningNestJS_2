2f59aae3d64b0bc0a0478d6e408a9473c0bd4e61
YuSeokHyun <fkstndnjs@naver.com>
2022-12-29 11:15:25 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import emailConfig from './config/emailConfig';
 import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.prod.env',
+      load: [emailConfig],
+      isGlobal: true,
     }),
   ],
   controllers: [AppController],
