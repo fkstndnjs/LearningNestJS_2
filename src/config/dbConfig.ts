0d17ea0ea22779d8bf432726da69cf651af15ad5
YuSeokHyun <fkstndnjs@naver.com>
2023-01-02 14:13:14 +0900

    fix

--- src/config/dbConfig.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-const dbConfig = registerAs('db', () => ({
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-}));
-
-export default dbConfig;
