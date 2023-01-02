9b116cf6fad921e962a7747c41cef69e9949a85e
YuSeokHyun <fkstndnjs@naver.com>
2023-01-02 14:22:10 +0900

    fix

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class UserEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+  })
+  id: number;
+
+  @Column({ length: 30 })
+  name: string;
+
+  @Column({ length: 60 })
+  email: string;
+
+  @Column({ length: 30 })
+  password: string;
+
+  @Column({ length: 60 })
+  signupVerifyToken: string;
+}
