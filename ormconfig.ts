7f3747fdd080e3df7bbbfeca19a1497815545547
YuSeokHyun <fkstndnjs@naver.com>
2023-01-02 16:08:19 +0900

    fix

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,7 +1,18 @@
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
+
+console.log('🚀 ---------------------------------------------------------🚀');
+console.log('🚀 ~ process.env.DATABASE_HOST', process.env.DATABASE_HOST);
+console.log('🚀 ---------------------------------------------------------🚀');
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: 'localhost'
-    port: 3306
-})
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: 3306,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'test',
+  entities: [__dirname + '/**/*.entity{.ts, .js}'],
+  synchronize: false,
+  migrations: [__dirname + '/**/migrations/*.js'],
+  migrationsTableName: 'migrations',
+});
