686970b93cc534a1cae81ff9a8f34a6d6aff44d5
YuSeokHyun <fkstndnjs@naver.com>
2023-01-02 14:12:42 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import dbConfig from './config/dbConfig';
 import emailConfig from './config/emailConfig';
 import { UsersModule } from './users/users.module';
 
@@ -14,7 +15,7 @@ import { UsersModule } from './users/users.module';
         process.env.NODE_ENV === 'dev'
           ? `src/env/.dev.env`
           : `src/env/.prod.env`,
-      load: [emailConfig],
+      load: [emailConfig, dbConfig],
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
