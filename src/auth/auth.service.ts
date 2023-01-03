955aa62779a078c08b116948ed3dcc09a43d0cee
YuSeokHyun <fkstndnjs@naver.com>
2023-01-03 15:51:10 +0900

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import jwt from 'jsonwebtoken';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login(user: { id: number; name: string; email: string }) {
+    return jwt.sign(user, 'secret', {
+      expiresIn: '1d',
+    });
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -92,7 +92,19 @@ export class UsersService {
   async login(body: UserLoginDto) {
     const { email, password } = body;
 
-    throw new Error('Method not implemented');
+    const user = await this.userRepository.findOne({
+      where: { email, password },
+    });
+
+    if (!user) {
+      throw new NotFoundException('유저가 존재하지 않습니다.');
+    }
+
+    return await this.authService.login({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
   }
 
   async getUserInfo(userId: number) {
