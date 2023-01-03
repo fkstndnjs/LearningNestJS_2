9f7672b32cdbae49da19bdfada6ce20cd02baf3f
YuSeokHyun <fkstndnjs@naver.com>
2023-01-03 15:38:12 +0900

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: true }),
+    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1d' } }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
