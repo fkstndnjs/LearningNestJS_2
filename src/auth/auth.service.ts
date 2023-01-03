d93525293ac75e422694fa3a8ffc1e5c5e7f3550
YuSeokHyun <fkstndnjs@naver.com>
2023-01-03 15:53:30 +0900

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import jwt from 'jsonwebtoken';
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
   login(user: { id: number; name: string; email: string }) {
-    return jwt.sign(user, 'secret', {
-      expiresIn: '1d',
-    });
+    return this.jwtService.sign(user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -100,7 +100,7 @@ export class UsersService {
       throw new NotFoundException('유저가 존재하지 않습니다.');
     }
 
-    return await this.authService.login({
+    return this.authService.login({
       id: user.id,
       name: user.name,
       email: user.email,
